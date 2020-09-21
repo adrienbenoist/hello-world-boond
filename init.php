419166c99b2fe0ab9983b400f52f73020b03a0ae
Adrien Benoist <adrien.benoist45@gmail.com>
2020-09-21 19:04:40 -0400

    First commit

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
-define('APP_KEY', '');
-define('APP_REFERENCE', '');
-define('APP_CODE', '');
+define('APP_KEY', '18a6af30e4161311cd49');
+define('APP_REFERENCE', 'APP1420');
+define('APP_CODE', 'helloworld');
